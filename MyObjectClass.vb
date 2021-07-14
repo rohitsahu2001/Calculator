a884399378248adbd0278bca8f2ca5fbed9c5ac2
rohitsahu2001 <83725536+rohitsahu2001@users.noreply.github.com>
2021-07-14 14:02:32 +0530

    Add files via upload

---
+++ MyObjectClass.vb
@@ -0,0 +1,22 @@
+﻿Public Class MyObjectClass
+    Function Plus(ByVal FNumber As Double, ByVal SNumber As Double) As Double
+        Dim Result As Double
+        Result = FNumber + SNumber
+        Plus = Result
+    End Function
+    Function Minus(ByVal FNumber As Double, ByVal SNumber As Double) As Double
+        Dim Result As Double
+        Result = FNumber - SNumber
+        Minus = Result
+    End Function
+    Function Multiply(ByVal FNumber As Double, ByVal SNumber As Double) As Double
+        Dim Result As Double
+        Result = FNumber * SNumber
+        Multiply = Result
+    End Function
+    Function Division(ByVal FNumber As Double, ByVal SNumber As Double) As Double
+        Dim Result As Double
+        Result = FNumber / SNumber
+        Division = Result
+    End Function
+End Class
